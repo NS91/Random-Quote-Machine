4bd1edf6d4b503b3a708cfcea209079789c413ab
Nima <nimaseyf@gmail.com>
2017-04-19 14:21:08 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
+// Binds the getQuote() function to the #quoter button
 $('#quoter').on('click', function(){
   getQuote();
 });
-
+// Defines the getQuote() function and twitter functionality
 function getQuote() {
  const url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?" 
   $.getJSON(url, function(data){
@@ -10,5 +11,5 @@ function getQuote() {
     $('.twitter-share-button').attr('href', "https://twitter.com/intent/tweet?text=" + data.quoteText + " - " + data.quoteAuthor); 
   });
 };
-
+// Calls getQuote() when the app opens to ensure it already displays a quote
 getQuote();
